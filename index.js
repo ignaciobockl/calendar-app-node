ab7abf89c9a0d0b9e1c45ce209a0055eb708d6d7
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-11 13:04:54 -0300

    installation and first configurations of express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Prueba nodemon')
+const express = require('express');
+
+
+const app = express();
+const port = 4000;
+
+
+// Public directory
+app.use( express.static('public') );
+
+app.listen( port, () => {
+    console.log( 'Server run on port: ', port );
+});
