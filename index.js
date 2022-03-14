e1820852eb206dae8d0da2310fb81f2f1da59fd3
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-03-13 21:34:21 -0300

    configure cors

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
 
 require('dotenv').config();
 
 
+
 const app = express();
 const port = process.env.PORT || 8000;
+
+// Configure cors
+app.use( cors() );
 
 // Database connection
 dbConnection();
